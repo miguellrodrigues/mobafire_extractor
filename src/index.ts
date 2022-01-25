fc6c667582b6b1325293e7024dd2d7cb00538163
Miguellr <miguellukas52@gmail.com>
2022-01-25 12:27:03 -0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,18 @@ import { get_champion_builds_from_mobafire } from './services/mobafire';
 
 /* Extract all */
 
-/*const champions = JSON.parse(fs.readFileSync('src/data/champions.json', 'utf8'));
+const champions = JSON.parse(fs.readFileSync('src/data/champions.json', 'utf8'));
 
 async function extract_all() {
  await get_champion_builds_from_mobafire(champions);
 }
 
-extract_all();*/
+extract_all();
 
 /* Extract One */
 
-async function extract_one(url: string) {
+/*async function extract_one(url: string) {
   await generate_lol_build_file_from_url(url);
 }
 
-extract_one('https://www.mobafire.com/league-of-legends/build/12-01-desperate-nasus-challenger-nasus-mid-in-depth-guide-605021');
+extract_one('https://www.mobafire.com/league-of-legends/build/12-01-desperate-nasus-challenger-nasus-mid-in-depth-guide-605021');*/
